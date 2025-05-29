cdccde6025ce37492e944007eae9ca37ee52fb2d
tsukimiyo <tsukimiyo0202@gmail.com>
2025-05-29 16:11:33 +0800

    completed main.py

--- Carla Python API/main.py
+++ Carla Python API/main.py
@@ -13,25 +13,41 @@ from CarlaEnv import CarlaEnv
 from CamManager import CamManager
 from DQN import DQNAgent
 
+
 def main():
+    ACTIONS = [
+        [0.5, 0.0, 0.0],   # go straight
+        [0.5, -0.3, 0.0],  # turn left
+        [0.5, 0.3, 0.0],   # turn right
+        [0.0, 0.0, 1.0]    # brake
+    ]
+    state_size = 4
+    action_size = 4
+    
     env = CarlaEnv()
     cam_manager = CamManager()
     detector = YOLODetector()
-    agent = DQNAgent()
+    agent = DQNAgent(state_size, action_size)
+    agent.load("./model/dqn_ep490.pth") # modify this for other paths
     
     try:
         env.spawn_vehicle()
         env.attach_camera(cam_manager.process_img)
-        controller = CarlaControl(env.vehicle)
+        state = [0.0, 0, 0, 0, 100.0]
+        print("\nPress Crtl + C to stop.\n")
+        try:
+            while True:
+                if cam_manager.latest_frame is None:
+                    continue
+                frame = cam_manager.latest_frame
+                detections = detector.detect(frame)
+                action_index = agent.choose_action(state)
+                action = ACTIONS[action_index]
 
-        while True:
-            if cam_manager.latest_frame is None:
-                continue
-            # frame = cam_manager.latest_frame
-            # detections = detector.detect(frame)
-            # state = get state from YOLO
-            # action = DQN choose action
-            # controller.apply_action(action)
+                next_state, reward, done, _ = env.step(action, detections)
+                state = next_state
+        except KeyboardInterrupt:
+            print("\nExiting\n")
     finally:
         env.cleanup()
 
